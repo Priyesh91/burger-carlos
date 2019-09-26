dedc6e50ec1a1ec50a84527bb49589467159d9ff
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-09-26 18:48:22 -0400

    update

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,50 @@
+var burger = require("../models/burger.js");
+
+module.exports = function (app) {
+
+	app.get("/", function (req, res) {
+		burger.selectAll("burgers", function (data) {
+			var burgerDataObj = {
+				burgers: data
+			};
+			console.log(burgerDataObj);
+			res.render("index", burgerDataObj);
+		});
+	});
+
+	app.post("/api/burgers", function (req, res) {
+		burger.insertOne("burger_name", [req.body.burger], function (err, result) {
+			if (err) {
+				console.log(err)
+			}
+			console.log("hitting post route")
+			res.redirect("/");
+		});
+	});
+
+	app.put("/api/burgers/:id", function (req, res) {
+		var condition = "id = " + req.params.id;
+		console.log("condition", condition);
+		console.log(req.params.id);
+		burger.updateOne({ devoured: req.body.devoured }, condition, function (result) {
+			if (result.changedRows == 0) {
+				return res.status(404).end();
+			} else {
+				res.status(200).end();
+			}
+		});
+	});
+
+	app.delete("/api/burgers/:id", function (req, res) {
+		var condition = "id = " + req.params.id;
+		console.log(condition);
+		burger.deleteOne(condition, function (result) {
+			if (result.changeRows === 0) {
+				// If no rows were changed, then the ID must not exist, so 404
+				return res.status(404).end();
+			} else {
+				res.status(200).end();
+			}
+		});
+	});
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+require('./controllers/burgers_controller')(app);
+
+app.listen(PORT);
